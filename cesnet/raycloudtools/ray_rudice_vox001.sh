e2496c6bda572c3cba77248d25efd14e3971ab9d
Martin Krucek <krucek@vukoz.cz>
2024-10-22 15:08:57 +0200

    aa

--- cesnet/raycloudtools/ray_rudice_vox001.sh
+++ cesnet/raycloudtools/ray_rudice_vox001.sh
@@ -1,8 +1,8 @@
 # define file name without extension
-DATA=SLP_Rudice_vox001_clip_gpst
+DATA=SLP_Rudice_clip_vox001_gpst
 
 # define a DATADIR variable
-DATADIR=/storage/plzen1/home/krucek/gs-lc/001 # substitute username and path to your real username and path
+DATADIR=/storage/plzen1/home/krucek/gs-lcr/001 # substitute username and path to your real username and path
 
 LOG_FILE="$DATADIR/$DATA.info.txt"
 
@@ -10,20 +10,29 @@ LOG_FILE="$DATADIR/$DATA.info.txt"
 
 #create info file
 echo "$PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR" >> $LOG_FILE
+echo "$(date) processing started" >> $LOG_FILE
 
 # test if the scratch directory is set
 test -n "$SCRATCHDIR" || { echo >&2 "Variable SCRATCHDIR is not set!"; exit 1; }
 
 #loads singularity
-module add singul/ || echo "singularity not loaded" >> $DATADIR/jobs_info.txt
+module add singul/ || echo "singularity not loaded" >> $LOG_FILE
+
+echo "$(date) singularity loaded" >> $LOG_FILE
 
 # copy input file and raycloudtools to scratch
 SOURCE_DATA="${DATA}.laz"
-cp /storage/plzen1/home/krucek/raycloudtools.img $SCRATCHDIR 
+cp /storage/plzen1/home/krucek/singularity_img/raycloudtools.img $SCRATCHDIR 
 cp $DATADIR/$SOURCE_DATA  $SCRATCHDIR 
 
 # move into scratch directory
 cd $SCRATCHDIR
+
+echo "$(date) data copied" >> $LOG_FILE
+echo "lof in SCRATCHDIR:" >> $LOG_FILE
+echo "$(ls)" >> $LOG_FILE
+echo "" >> $LOG_FILE
+
 
 # init files names
 DATA_PLY="${DATA}.ply"
@@ -35,19 +44,32 @@ TREES_TXT="${DATA}_trees.txt"
 TREES_MESH_PLY="${DATA}_trees_mesh.ply"
 LEAVES_PLY="${DATA}_leaves.ply"
 
+echo "$(date) raycloudtools processing start" >> $LOG_FILE
 # RUN raycloudtools in singularity to process the data
 singularity exec -B $SCRATCHDIR/:/data ./raycloudtools.img rayimport $SOURCE_DATA ray 0,0,-10 --remove_start_pos
+echo "$(date) loaded" >> $LOG_FILE
 singularity exec -B $SCRATCHDIR/:/data ./raycloudtools.img rayextract terrain $DATA_PLY
+echo "$(date) terrain extracted" >> $LOG_FILE
 singularity exec -B $SCRATCHDIR/:/data ./raycloudtools.img rayextract trunks $DATA_PLY
+echo "$(date) trunks extracted" >> $LOG_FILE
 singularity exec -B $SCRATCHDIR/:/data ./raycloudtools.img rayextract forest $DATA_PLY
+echo "$(date) forest extracted" >> $LOG_FILE
 singularity exec -B $SCRATCHDIR/:/data ./raycloudtools.img rayextract trees $DATA_PLY $TERRAIN_PLY
+echo "$(date) trees extracted" >> $LOG_FILE
 singularity exec -B $SCRATCHDIR/:/data ./raycloudtools.img rayextract leaves $DATA_PLY $TREES_TXT
+echo "$(date) leaves extracted" >> $LOG_FILE
+
+echo "lof in SCRATCHDIR:" >> $LOG_FILE
+echo "$(ls -lh)" >> $LOG_FILE
+echo "" >> $LOG_FILE
 
 # Create images for each segmented tree
 SEGMENT_DIR="${SCRATCHDIR}/segments"
 mkdir -p $SEGMENT_DIR
 cp $SEGMENTED_PLY segments/$SEGMENTED_PLY
 singularity exec -B $SCRATCHDIR/:/data ./raycloudtools.img raysplit segments/$SEGMENTED_PLY seg_colour
+
+echo "$(date) segments extracted" >> $LOG_FILE
 
 for segment_file in ${SEGMENT_DIR}/*.ply; do
     singularity exec -B $SCRATCHDIR/:/data ./raycloudtools.img rayrender "$segment_file" right ends
@@ -57,10 +79,13 @@ for segment_file in ${SEGMENT_DIR}/*.ply; do
     #echo "Rendered image for $segment_file" >> $LOG_FILE
 done
 
+echo "$(date) segments exported" >> $LOG_FILE
+
 # remove files which are no to be copied
 rm $SOURCE_DATA 
 rm rm raycloudtools.img
 #find segments/ -type f -name "*.ply" -delete
+rm segments/*.txt
 rm $DATA_PLY
 rm segments/$DATA_PLY
 
@@ -68,10 +93,14 @@ rm segments/$DATA_PLY
 ZIP_NAME="${DATA}_results.zip"
 zip -r "$ZIP_NAME" .
 
+echo "$(date) compressed" >> $LOG_FILE
+
 # Copy the ZIP file to $DATADIR
 cp "$ZIP_NAME" $DATADIR
-echo "Copied $ZIP_NAME to $DATADIR" >> $LOG_FILE
+echo "$(date) Copied $ZIP_NAME to $DATADIR" >> $LOG_FILE
 
 
 # clean the SCRATCH directory
 clean_scratch
+echo "$(date) clean_scratch" >> $LOG_FILE
+
--- cesnet/readme.md
+++ cesnet/readme.md
@@ -1,39 +1,22 @@
-code used to run on cesnet infrastructure
+## code used to run on cesnet infrastructure
 
+#### Ask interactive job
+```
+qsub -I -l select=1:ncpus=6:mem=8gb:scratch_local=10gb -l walltime=2:00:00
+```
 
-cp /storage/plzen1/home/krucek/ray_processing/singularityfile $SCRATCHDIR
-cd $SCRATCHDIR
+#### Build docker images for singularity
+```
+singularity build raycloudtools.img docker://tdevereux/raycloudtools:latest
+singularity build r-lidar-tools.img docker://martinkrucek/r-lidar-tools:latest
+singularity build pdal.img docker://pdal/pdal:latest
+singularity build lastools.img docker://hakonamdal/lastools:latest
+singularity build cloudcompare.img docker://tswetnam/cloudcompare:latest
+```
 
-singularity build --remote r-lidar-tools.sif singularityfile
-
-docker build -t helios .
-
-cp /storage/plzen1/home/krucek/InterCOST/src/rtest.R $SCRATCHDIR
-cp /storage/plzen1/home/krucek/InterCOST/img/r-lidar-tools.img $SCRATCHDIR
-
-singularity exec -B $SCRATCHDIR/:/data ./r-lidar-tools.img Rscript /data/rtest.R
-
-
-cp /storage/plzen1/home/krucek/InterCOST/src/create_pdal_pipeline.sh $SCRATCHDIR
-chmod +x create_pdal_pipeline.sh
-./create_pdal_pipeline.sh "$SOURCE_DATA" "$PDAL_FILE" "$VOXELIZE" "$ADD_TIME" "$VOX_RES"
-cat pdal_pipeline.json
-singularity exec -B $SCRATCHDIR/:/data ./pdal.img pdal pipeline /data/pdal_pipeline.json
-
-FILE_SIZE=$(du -h "$FILE_IN")
-echo "Original file size: $FILE_SIZE" >> test.txt
-
-echo "Original file size2: $(du -h "$FILE_IN")" >> test.txt
-
-
-pipeline+=",{
-      \"type\": \"writers.las\",
-      \"compression\": \"laszip\",
-      \"filename\": \"$OUTPUT_FILE\",
-      \"minor_version\": 2
-    }
-
-
+####
+```
 qsub -l select=1:ncpus=24:mem=64gb:scratch_local=150gb -l walltime=24:00:00 /storage/plzen1/home/krucek/gs-lcr/001/ray_klepacov1_vox001.sh
 qsub -l select=1:ncpus=24:mem=64gb:scratch_local=150gb -l walltime=24:00:00 /storage/plzen1/home/krucek/gs-lcr/001/ray_klepacov2_vox001.sh
 qsub -l select=1:ncpus=24:mem=64gb:scratch_local=150gb -l walltime=24:00:00 /storage/plzen1/home/krucek/gs-lcr/001/ray_rudice_vox001.sh
+```
